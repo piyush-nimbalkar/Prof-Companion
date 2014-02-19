9e440326016a870283f020ca49ce330a28dc9551
shashank <shashankdevan@gmail.com>
2014-02-19 18:27:14 -0500

    Testing Git Push

--- src/com/example/diary/ContactActivity.java
+++ src/com/example/diary/ContactActivity.java
@@ -2,13 +2,19 @@ package com.example.diary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ContactActivity extends Activity {
 
-	@Override
+	//ImageButton editName;
+	TextView textViewName;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact);
+		textViewName = (TextView) findViewById(R.id.textViewName);
+		textViewName.setText("Testing Git");
 	}
 
 
