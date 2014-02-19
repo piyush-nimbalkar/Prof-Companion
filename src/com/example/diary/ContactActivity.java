37b0e304ec60c3cfa336a24625121cd738454a90
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-19 12:51:31 -0500

    New activity to display contact info

---
+++ src/com/example/diary/ContactActivity.java
@@ -0,0 +1,7 @@
+package com.example.diary;
+
+import android.app.Activity;
+
+public class ContactActivity extends Activity {
+
+}
--- src/com/example/diary/MainActivity.java
+++ src/com/example/diary/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.diary;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,11 +12,13 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements OnClickListener {
 
 	private Button contactButton;
+	private Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context = this;
 
 		contactButton = (Button) findViewById(R.id.contact_button);
 		contactButton.setOnClickListener(this);
@@ -24,7 +28,9 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch(v.getId()) {
 		case R.id.contact_button:
-			Toast.makeText(this, "Here is my contact!", Toast.LENGTH_LONG).show();
+			Toast.makeText(context, "Here is my contact!", Toast.LENGTH_LONG).show();
+			Intent i = new Intent(context, ContactActivity.class);
+			startActivity(i);
 			break;
 		case R.id.courses_button:
 			Toast.makeText(this, "Here are the courses I taught!", Toast.LENGTH_LONG).show();
