2f3432fd0c0dc7f1341a807d3a3106104b55eb72
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-19 00:32:24 -0500

    Recording clicks on various buttons

--- src/com/example/diary/MainActivity.java
+++ src/com/example/diary/MainActivity.java
@@ -2,21 +2,40 @@ package com.example.diary;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	private Button contactButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		contactButton = (Button) findViewById(R.id.contact_button);
+		contactButton.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.contact_button:
+			Toast.makeText(this, "Here is my contact!", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.courses_button:
+			Toast.makeText(this, "Here are the courses I taught!", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.events_button:
+			Toast.makeText(this, "Here are my events!", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.news_button:
+			Toast.makeText(this, "Here are my news!", Toast.LENGTH_LONG).show();
+			break;
+		}
 	}
 
 }
