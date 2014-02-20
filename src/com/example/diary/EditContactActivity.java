7daea9d55a4c8ceab316c8a6ba343acda7f527a1
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-19 23:00:13 -0500

    New activity for editing the contacts and a blank UI for that

---
+++ src/com/example/diary/EditContactActivity.java
@@ -0,0 +1,15 @@
+package com.example.diary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditContactActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_contact);
+
+	}
+
+}
