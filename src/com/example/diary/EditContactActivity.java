ad588f6ac6f9d0835679af15fa8fdc330ee677e1
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-20 00:00:28 -0500

    Taking the serialized object

--- src/com/example/diary/EditContactActivity.java
+++ src/com/example/diary/EditContactActivity.java
@@ -1,15 +1,21 @@
 package com.example.diary;
 
+import model.Contact;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class EditContactActivity extends Activity {
+
+	private Contact contact;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_contact);
 		
+		contact = (Contact) getIntent().getSerializableExtra("Contact");
+		Toast.makeText(this, contact.getEmail(), Toast.LENGTH_SHORT).show();
 	}
 
 }
