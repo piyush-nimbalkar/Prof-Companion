558f9d9c7485762c327163fafa7145ba7ce0a394
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-25 21:54:22 -0500

    Open the course screen on click of a particular course

--- src/com/example/diary/CourseListActivity.java
+++ src/com/example/diary/CourseListActivity.java
@@ -1,35 +1,42 @@
 package com.example.diary;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import model.Course;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-public class CourseListActivity extends Activity {
+public class CourseListActivity extends Activity implements OnItemClickListener {
 
+	private Context context;
 	private ArrayList<Course> courses = new ArrayList<Course>();
 	private ListView listview;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_course_list);
-
+		context = this;
 		listview = (ListView) findViewById(R.id.listViewCourses);
-
 		courses = getIntent().getParcelableArrayListExtra("Courses");
 
 		final CourseArrayAdaptor adapter = new CourseArrayAdaptor(this, courses);
 		listview.setAdapter(adapter);
+		listview.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Course course = (Course) parent.getItemAtPosition(position);
+		Intent i = new Intent(context, CourseActivity.class);
+		i.putExtra("Course", course);
+		startActivity(i);
 	}
 
 }
