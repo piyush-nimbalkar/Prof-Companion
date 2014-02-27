9216be0c7d409a92e5b17eb46fd4214dcc6a603c
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-27 00:37:56 -0500

    Edit icon on the action bar for course

--- src/com/example/diary/CourseActivity.java
+++ src/com/example/diary/CourseActivity.java
@@ -5,14 +5,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class CourseActivity extends Activity implements OnClickListener {
+public class CourseActivity extends Activity {
 
-	private final int REQUEST_CODE1 = 1;
+	private final int REQUEST_EDIT = 1;
 	final Context context = this;
 
 	private Course course;
@@ -24,8 +23,6 @@ public class CourseActivity extends Activity implements OnClickListener {
 	private TextView textViewCourseDays;
 	private TextView textViewCourseTime;
 
-	private Button buttonEditCourse;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,7 +33,6 @@ public class CourseActivity extends Activity implements OnClickListener {
 		textViewCourseCredits = (TextView) findViewById(R.id.textViewCourseCredits);
 		textViewCourseDays = (TextView) findViewById(R.id.textViewCourseDays);
 		textViewCourseTime = (TextView) findViewById(R.id.textViewCourseTime);
-		buttonEditCourse = (Button) findViewById(R.id.buttonEditCourse);
 
 		course = getIntent().getParcelableExtra("Course");
 		coursePosition = getIntent().getIntExtra("Position", 0);
@@ -46,25 +42,11 @@ public class CourseActivity extends Activity implements OnClickListener {
 		textViewCourseCredits.setText(course.getCreditHours());
 		textViewCourseDays.setText(course.getDays());
 		textViewCourseTime.setText(course.getTime());
-
-		buttonEditCourse.setOnClickListener(this);
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch(v.getId()) {
-		case R.id.buttonEditCourse:
-			Intent i = new Intent(context, EditCourseActivity.class);
-			i.putExtra("Course", course);
-			startActivityForResult(i, REQUEST_CODE1);
-			break;
-		}
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == REQUEST_CODE1 && resultCode == RESULT_OK){
+		if (requestCode == REQUEST_EDIT && resultCode == RESULT_OK){
 				course = (Course) data.getExtras().get("Course");
-
 				textViewCourseCode.setText(course.getCourseNumber());
 				textViewCourseName.setText(course.getCourseTitle());
 				textViewCourseCredits.setText(course.getCreditHours());
@@ -73,6 +55,26 @@ public class CourseActivity extends Activity implements OnClickListener {
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.course_details_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.edit_course:
+			Intent i = new Intent(context, EditCourseActivity.class);
+			i.putExtra("Course", course);
+			startActivityForResult(i, REQUEST_EDIT);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 	public void finish() {
 		Intent data = new Intent();
 		data.putExtra("Course", course);
